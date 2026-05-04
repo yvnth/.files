a3866ae789c4144a731dda10afc131f95dee7bc9
yash2k4 <yash2k4@users.noreply.github.com>
2026-05-05 04:06:53 +0530

    chore(sway): delete seperate swaylock config and add it in sway config

--- home/dots.nix
+++ home/dots.nix
@@ -18,7 +18,6 @@ in {
     ".config/shell".source = mkOutOfStoreSymlink "${configDir}/shell";
     ".config/starship".source = mkOutOfStoreSymlink "${configDir}/starship";
     ".config/sway".source = mkOutOfStoreSymlink "${configDir}/sway";
-    ".config/swaylock".source = mkOutOfStoreSymlink "${configDir}/swaylock";
     ".config/tmux".source = mkOutOfStoreSymlink "${configDir}/tmux";
     ".config/waybar".source = mkOutOfStoreSymlink "${configDir}/waybar";
     ".config/yazi".source = mkOutOfStoreSymlink "${configDir}/yazi";
