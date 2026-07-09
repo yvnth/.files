01a267d946e37885c52bcd9adcb3623716904fc2
yvnth <yvnth@users.noreply.github.com>
2026-07-09 16:13:46 +0530

    chore(nvidia): from stable to latest

--- modules/core/nvidia-intel.nix
+++ modules/core/nvidia-intel.nix
@@ -9,13 +9,8 @@
 
   config = lib.mkIf config.modules.nvidia-intel.enable {
     boot = {
-      initrd.kernelModules = [
-        "nvidia"
-        "nvidia_drm"
-        "nvidia_modeset"
-        "nvidia_uvm"
-      ];
       kernelPackages = pkgs.linuxPackages_latest;
+
       kernelParams = [
         "nvidia-drm.modeset=1"
         "nvidia-drm.fbdev=1"
@@ -24,36 +19,39 @@
 
     environment.sessionVariables = {
       NIXOS_OZONE_WL = "1";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      LIBVA_DRIVER_NAME = "iHD";
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      NVD_BACKEND = "direct";
       ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      LIBVA_DRIVER_NAME = "iHD";
     };
 
     hardware = {
       graphics = {
         enable = true;
         enable32Bit = true;
+
         extraPackages = with pkgs; [
           intel-media-driver
           intel-compute-runtime
         ];
       };
+
       nvidia = {
-        modesetting.enable = true;
-        nvidiaPersistenced = false;
-        nvidiaSettings = true;
         open = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        modesetting.enable = true;
+
+        nvidiaSettings = true;
+        nvidiaPersistenced = false;
+
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
+
         powerManagement = {
           enable = true;
           finegrained = true;
         };
+
         prime = {
           intelBusId = "PCI:0:2:0";
           nvidiaBusId = "PCI:1:0:0";
+
           offload = {
             enable = true;
             enableOffloadCmd = true;
